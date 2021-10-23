5925cf6958b35f3aa54784ffe4d1984ff13f8c5f
Dima <pechenenkod@gmail.com>
2021-10-23 19:10:38 +0300

    TC_added CurrenciesScreenStoryboard and ConverterScreenStoryboard

--- test_converter/Modules/FirstScreen/ViewController.swift
+++ test_converter/Modules/FirstScreen/ViewController.swift
@@ -11,9 +11,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.hideNavigationBar()
     }
 
+    @IBAction private func nextScreenCurrencies(_ sender: Any) {
+        let storyboard = UIStoryboard.init(name: "CurrenciesScreenVC", bundle: nil)
+        guard let currenciesVC = storyboard.instantiateViewController(identifier: "CurrenciesScreenVC") as? CurrenciesScreenVC else { return }
+        
+        navigationController?.pushViewController(currenciesVC, animated: true)
+    }
 
 }
 
+extension UIViewController {
+    func hideNavigationBar() {
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+
+    func showNavigationBar() {
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+}
